c628f455def3479de8b63f3e2e5df21578524cef
Abel Ianovici <ianovici.abel04@gmail.com>
2025-01-16 22:25:23 +0100

    add basic main

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "github.com/gocolly/colly/v2"
 
 func main() {
-
+	// go to wikipedia's golang page copy the 10 first lines and paste it in the terminal
+	c := colly.NewCollector(
+		colly.AllowedDomains("en.wikipedia.org"),
+	)
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
 }
