53292a374821cc8ce4687ce9dd7cd347c85d3961
Abel Ianovici <ianovici.abel04@gmail.com>
2025-02-06 22:43:58 +0100

    protobuf

--- main.go
+++ main.go
@@ -4,8 +4,33 @@ import (
 	"fmt"
 	"os"
 	"net/http"
+	"google.golang.org/protobuf/proto"
 	"github.com/gocolly/colly/v2"
 )
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("No URL to scrape")
+		os.Exit(1)
+	}
+	url := os.Args[1]
+	if !isReachable(url) {
+		fmt.Println("URL is not reachable")
+		os.Exit(1)
+	}
+	fmt.Println("URL is reachable")
+	var ruleSet map[string]int
+	if len(os.Args) > 2 {
+		rulePath := os.Args[2]
+		fmt.Println(rulePath)
+		ruleSet = parseRuleSet(rulePath)
+	}
+	scrap(url, ruleSet)
+}
+
+func parseRuleSet(string rulePath) map[string]int {
+
+}
 
 func isReachable(url string) bool {
 	// check if url is valid
@@ -19,39 +44,21 @@ func isReachable(url string) bool {
 	}
 }
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("No URL to scrape")
-		os.Exit(1)
-	}
-	url := os.Args[1]
-	fmt.Println(url)
-	if !isReachable(url) {
-		fmt.Println("URL is not reachable")
-		os.Exit(1)
-	}
-	fmt.Println("URL is reachable")
-	scrapUrl := "https://en.wikipedia.org/wiki/Go_(programming_language)"
-	c := colly.NewCollector(
-		colly.AllowedDomains("en.wikipedia.org"),
-	)
+func scrap(url string, ruleSet map[string]int) bool {
+	c := colly.NewCollector()
 
 	c.OnRequest(func(r *colly.Request) {
-		//fmt.Printf("Visiting %s\n", r.URL)
+		fmt.Printf("Visiting %s\n", r.URL)
 	})
 
-	// go to wikipedia's golang page copy the first paragraphe and paste it in the terminal
-	count := 0
 	c.OnHTML("p", func(e *colly.HTMLElement) {
-		if 0 < count && count < 2{
-			//fmt.Println(e.Text)
-		}
-		count++
+		fmt.Println(e.Text)
 	})
 
 	c.OnError(func(r *colly.Response, err error) {
-		//fmt.Printf("Error while scraping %s\n", err.Error())
+		fmt.Printf("Error while scraping %s\n", err.Error())
 	})
 
-	c.Visit(scrapUrl)
+	c.Visit(url)
+	return true
 }
