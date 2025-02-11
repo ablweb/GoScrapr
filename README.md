e212cfd9c24b684845f2899800416afef6486eab
Abel Ianovici <ianovici.abel04@gmail.com>
2025-02-11 19:36:27 +0100

    fixed the priority

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # GoScrapr
 GoScrapr is a web scraper tool written in go
 
+# how to use
+```bash
+./goscrapr <url> [rule set]
+
+```
+The rule set is a json file, look at ruleSet.json for the structure 
+
 ## TODO
 1. this is the core tool, there should be an api to use it, so no all prints should be replaced by error code except for the text scraped, which should be passed to the cout
 2. rule priority functionnality
--- main.go
+++ main.go
@@ -5,14 +5,16 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sort"
+
 	"github.com/gocolly/colly/v2"
 )
 
-var Reset = "\033[0m" 
-var Error = "\033[31m" 
-var Succes = "\033[32m" 
-var Warning = "\033[33m" 
-var Info = "\033[34m" 
+var Reset = "\033[0m"
+var Error = "\033[31m"
+var Succes = "\033[32m"
+var Warning = "\033[33m"
+var Info = "\033[34m"
 
 type Rule struct {
 	Query    string `json:"query"`
@@ -23,16 +25,16 @@ type RuleSet []Rule
 func main() {
 	// No URL
 	if len(os.Args) < 2 {
-		fmt.Println(Error+"No URL to scrape"+Reset)
+		fmt.Println(Error + "No URL to scrape" + Reset)
 		os.Exit(1)
 	}
 	url := os.Args[1]
 	// Not valid URL
 	if !isReachable(&url) {
-		fmt.Println(Error+"URL is not reachable"+Reset)
+		fmt.Println(Error + "URL is not reachable" + Reset)
 		os.Exit(1)
 	}
-	fmt.Println(Succes+"URL is reachable"+Reset)
+	fmt.Println(Succes + "URL is reachable" + Reset)
 	var rules RuleSet
 	if len(os.Args) > 2 {
 		rulesPath := os.Args[2]
@@ -43,25 +45,26 @@ func main() {
 		}
 		err_U := json.Unmarshal(jsonContent, &rules)
 		if err_U != nil {
-      fmt.Printf(Error+"Error unmarshalling JSON: %s\n"+Reset, err_U)
+			fmt.Printf(Error+"Error unmarshalling JSON: %s\n"+Reset, err_U)
 			os.Exit(1)
 		}
 	} else {
 		rules = nil
-		fmt.Println(Warning+"No rules provided. Scraping URL without any rules."+Reset)
+		fmt.Println(Warning + "No rules provided. Scraping URL without any rules." + Reset)
 	}
-	fmt.Print(scrap(&url, &rules))
+	scraped, _ := scrap(&url, &rules)
+	fmt.Print(scraped)
 }
 
 func isReachable(url *string) bool {
 	// check if url is valid
-	fmt.Print(Info+"Checking URL: "+Reset)
+	fmt.Print(Info + "Checking URL: " + Reset)
 	resp, err := http.Get(*url)
 	if err != nil {
-		print(Error+err.Error()+"\n"+Reset)
+		print(Error + err.Error() + "\n" + Reset)
 		return false
 	} else {
-		print(Info+fmt.Sprint(resp.StatusCode)+resp.Status+"\n"+Reset)
+		print(Info + fmt.Sprint(resp.StatusCode) + resp.Status + "\n" + Reset)
 		return true
 	}
 }
@@ -73,16 +76,16 @@ func scrap(url *string, rules *RuleSet) (string, error) {
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Printf(Info+"Visiting: %s\n"+Reset, r.URL)
 	})
-	if len(*rules) == 0  {
+	if len(*rules) == 0 {
 		// Scrap everything
 		c.OnHTML("html", func(e *colly.HTMLElement) {
-			dataMap[0] += e.Text+"\n"
+			dataMap[0] += e.Text + "\n"
 		})
 	} else {
 		// Scrap with rules
 		for _, rule := range *rules {
 			c.OnHTML(rule.Query, func(e *colly.HTMLElement) {
-				dataMap[rule.Priority] += e.Text+"\n"
+				dataMap[rule.Priority] += e.Text + "\n"
 			})
 		}
 	}
@@ -93,10 +96,15 @@ func scrap(url *string, rules *RuleSet) (string, error) {
 	if err := c.Visit(*url); err != nil {
 		return "", err
 	}
-	
+
 	var scraped string
-	for _, data := range dataMap {
-		scraped += data
+	var keys []int
+	for k := range dataMap {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		scraped += dataMap[k]
 	}
 	return scraped, nil
 }
